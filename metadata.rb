dc9418ef78c4529668f0a3013e804e6649f1d2fa
Jerry Jackson <schade@users.noreply.github.com>
2014-08-06 19:38:54 -0500

    Update metadata.rb
    Making some naming adjustments to move this cookbook via refactor into three cookbooks: strongSwan-server, strongSwan-pki, and strongSwan-scenarios.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             'ipsec-pki'
+name             'strongSwan-pki'
 maintainer       'Jerry Jackson'
 maintainer_email 'jerry.jackson@acommitteeoflunatics.com'
 license          'Apache v2'
-description      'Installs/Configures ipsec-pki'
-long_description 'Installs/Configures ipsec-pki'
+description      'Installs/Configures strongSwan-pki'
+long_description "Installs/Configures strongSwan-pki for proper handling of your CA and it's associated keys and certs"
 version          '0.1.0'
 
-depends 'strongSwan'
+depends 'strongSwan-server'
