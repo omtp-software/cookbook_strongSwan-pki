028ffeb2a7b42e75a0d1d44ba755b1c4a96a44a4
Jerry Jackson <jerry.jackson@acommitteeoflunatics.com>
2014-08-08 16:25:23 -0500

    Adding some necessary bits for release automation to work properly.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'strongSwan-pki'
 maintainer       'Jerry Jackson'
 maintainer_email 'jerry.jackson@acommitteeoflunatics.com'
-license          'Apache v2'
-description      'Installs/Configures strongSwan-pki'
-long_description "Installs/Configures strongSwan-pki for proper handling of your CA and it's associated keys and certs"
-version          '0.1.0'
+license          'Apache v2.0'
+description      'LWRP that provides access to ipsec pki commands for use with strongSwan-base.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
 depends 'strongSwan-base'
